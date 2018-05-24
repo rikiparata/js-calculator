5e2cd3ebea536cd997a1392e896f3f94ee179560
Riki Parata <you@example.com>
2018-05-25 00:26:41 +1200

    calculator js

---
+++ calc.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', start)
+
+function start() {
+    addEventListeners()
+}
+function addEventListeners() {
+
+    var buttons = document.getElementsByClassName("buttons")
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", calculate)
+     }
+    }
+    var temp = '';
+    var entries = []; 
+
+    function calculate (evt) {
+        var val = evt.target.id  //add button text to value
+
+  if (val === 'AC') {
+    temp = '';
+    entries = [];
+    document.getElementById("answer").innerHTML = temp;
+  }
+  else if (val === 'C') {
+    temp = temp.slice(0, - 1);
+    document.getElementById("answer").innerHTML = temp;
+  }
+  else if (val === 'equal') {
+    var answer = eval(entries.join(''));
+    var compute = answer;
+    document.getElementById("answer").innerHTML = compute;
+    temp = compute;
+
+  } else {
+    temp += val;
+    entries.push(val);
+    document.getElementById("answer").innerHTML = temp;
+  }
+}
